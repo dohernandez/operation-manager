4bec35db3e8aa2427354246fec422fc14aed785b
Darien Hernandez <dh@hellofresh.com>
2017-11-12 00:24:33 +0100

    Added Manager Bundle

---
+++ src/ManagerBundle/Entity/OperationType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ManagerBundle\Entity;
+
+/**
+ * OperationType
+ */
+class OperationType
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return OperationType
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
