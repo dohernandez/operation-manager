7e72fb64edb6f6354cc0e079a72107e47feade60
Darien Hernandez <dh@hellofresh.com>
2017-11-15 11:52:28 +0100

    Created entity ActionType using sf3 doctrine:generate:entity command

---
+++ src/ManagerBundle/Entity/ActionType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ManagerBundle\Entity;
+
+/**
+ * ActionType
+ */
+class ActionType
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return ActionType
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
