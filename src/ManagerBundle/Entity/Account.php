9da696128b9f1084d74f7afc31e6873f47cb3191
Darien Hernandez <dh@hellofresh.com>
2017-11-20 01:31:20 +0100

    Added relation between Transfer and Account.

--- src/ManagerBundle/Entity/Account.php
+++ src/ManagerBundle/Entity/Account.php
@@ -2,6 +2,8 @@
 
 namespace ManagerBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * Account
  */
@@ -10,6 +12,62 @@ class Account extends Entity
     use EntityName;
     use EntityIBAN;
     use EntityType;
+
+    /**
+     * @var ArrayCollection[Transfer]
+     */
+    private $entrances;
+
+    /**
+     * @var ArrayCollection[Transfer]
+     */
+    private $exits;
+
+    public function __construct()
+    {
+        $this->entrances = new ArrayCollection();
+        $this->exits = new ArrayCollection();
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getEntrances(): ArrayCollection
+    {
+        return $this->entrances;
+    }
+
+    /**
+     * @param ArrayCollection $entrances
+     *
+     * @return Account
+     */
+    public function setEntrances(ArrayCollection $entrances)
+    {
+        $this->entrances = $entrances;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getExits(): ArrayCollection
+    {
+        return $this->exits;
+    }
+
+    /**
+     * @param ArrayCollection $exits
+     *
+     * @return Account
+     */
+    public function setExits(ArrayCollection $exits)
+    {
+        $this->exits = $exits;
+
+        return $this;
+    }
 
     public function __toString()
     {
